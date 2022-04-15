aa8c0df092aba3bb0e89f5deb930c24b372b26ed
kim.jong.rok <kim.jong.rok@danbieud.co.kr>
2022-04-15 19:51:46 +0900

    @curation -> @DanbiEduCorp github org 와 일치해야 함

--- packages/mobile/pages/index.tsx
+++ packages/mobile/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import {Page} from '@curation/ui/stories/Page'
+import {Page} from '@DanbiEduCorp/ui/stories/Page'
 
 const Home: NextPage = () => {
   return (

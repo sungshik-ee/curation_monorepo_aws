96c118084d4b18b57d91533f299a1a6f9760ebe5
kim.jong.rok <kim.jong.rok@danbieud.co.kr>
2022-04-18 13:44:53 +0900

    dev 및 build lerna 대신 yarn workspace 변경 yarn berry (latest) 설정

--- packages/ui/stories/Page2.tsx
+++ packages/ui/stories/Page2.tsx
@@ -18,7 +18,7 @@ export const Page: React.FC = () => {
             />
 
             <section>
-                <h2>Pages in Storybook Page2.tsx changed</h2>
+                <h2>Pages in Storybook 20220418 !</h2>
                 <p>
                     We recommend building UIs with a{' '}
                     <a href="https://componentdriven.org" target="_blank" rel="noopener noreferrer">
